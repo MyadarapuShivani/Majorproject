550409ceb39151d1af6fefbed1d7c88c77da782d
MyadarapuShivani <114327005+MyadarapuShivani@users.noreply.github.com>
2022-09-25 10:56:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model_nb = joblib.load('POEM-CLASSIFICATION')
+st.title('POEM CLASSIFICATION') #creates a title in web app
+ip = st.text_input('Enter POEM:') #creates a text box in web app
+op = model_nb.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
